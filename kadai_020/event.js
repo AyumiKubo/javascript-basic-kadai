bf7c576e7e039a698480503c11f84080d3fa7ee6
AyumiKubo <72365668+AyumiKubo@users.noreply.github.com>
2025-03-02 17:29:20 +0900

    再提出のため、ファイル差し替え

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,8 @@
 // btnというidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
+
+// testいうidを持つ要素を定数に代入する
+const text = document.getElementById('text')
  
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
